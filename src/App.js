aa778a918805d4028a24aece8eaa4cf2a3123735
Fer Vittorello <fvittorello@gmail.com>
2020-10-20 02:16:55 -0300

    feat(HomePage) add homepage component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import { HomePage } from 'components/HomePage';
+
 export function App() {
-	return <div>Hello Commerce</div>;
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
