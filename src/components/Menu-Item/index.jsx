93d13333876c4802a08e309c3782299aea1ad665
Fer Vittorello <fvittorello@gmail.com>
2020-10-20 15:47:09 -0300

    chore(Menu-Item) add missing styles and animations on hover

--- src/components/Menu-Item/index.jsx
+++ src/components/Menu-Item/index.jsx
@@ -3,11 +3,13 @@ import './Menu-Item.styles.scss';
 
 export const MenuItem = ({ title, imageUrl, size }) => {
 	return (
-		<div
-			style={{
-				backgroundImage: `url(${imageUrl})`,
-			}}
-			className={`${size} menu-item`}>
+		<div className={`${size} menu-item`}>
+			<div
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+				}}
+				className='background-image'
+			/>
 			<div className='content'>
 				<h1 className='title'>{title}</h1>
 				<span className='subtitle'>SHOP NOW</span>
