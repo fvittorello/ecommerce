1bd611365a6fe688153ca69aee346e30dda35236
Fer Vittorello <fvittorello@gmail.com>
2020-10-21 18:21:07 -0300

    feat(CollectionItem) add new component and styles

--- src/components/CollectionPreview/index.jsx
+++ src/components/CollectionPreview/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { CollectionItem } from 'components/CollectionItem';
+
+import './preview-collection.styles.scss';
+
 export default function CollectionPreview({ title, items }) {
 	return (
 		<div className='collection-preview'>
@@ -7,8 +11,8 @@ export default function CollectionPreview({ title, items }) {
 			<div className='preview'>
 				{items
 					.filter((item, idx) => idx < 4)
-					.map((item) => (
-						<div key={item.id}>{item.name}</div>
+					.map(({ id, ...otherItemProps }) => (
+						<CollectionItem key={id} {...otherItemProps} />
 					))}
 			</div>
 		</div>
