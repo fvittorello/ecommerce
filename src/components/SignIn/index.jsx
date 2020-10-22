d4d18008467478976cb7c2490b010c9a4bf4d806
Fer Vittorello <fvittorello@gmail.com>
2020-10-22 18:17:18 -0300

    feat(SignIn) add signin with mail and password that works with the db

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { FormInput } from 'components/FormInput';
 import { CustomButton } from 'components/CustomButton';
 
-import { signInWithGoogle } from 'firebase/firebase.utils';
+import { auth, signInWithGoogle } from 'firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,8 +17,17 @@ export class SignIn extends Component {
 		};
 	}
 
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
+
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.error(error);
+		}
 
 		this.setState({ email: '', password: '' });
 	};
