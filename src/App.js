15611f19ca1bd0a06a5949f25ed4bede40dd5911
Fer Vittorello <fvittorello@gmail.com>
2020-10-22 18:08:54 -0300

    feat(firebase signup) add firebase signup with email and password

--- src/App.js
+++ src/App.js
@@ -42,6 +42,8 @@ class App extends React.Component {
 							...snapShot.data(),
 						},
 					});
+
+					console.log(this.state);
 				});
 			} else {
 				this.setState({ currentUser: userAuth });
--- src/pages/SignIn-SignUp/index.jsx
+++ src/pages/SignIn-SignUp/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { SignIn } from 'components/SignIn';
+import SignUp from 'components/SignUp';
 
 import './signin-signup.styles.scss';
 
@@ -8,6 +9,7 @@ export const SignInAndSignUpPage = () => {
 	return (
 		<div className='sign-in-and-sign-up'>
 			<SignIn />
+			<SignUp />
 		</div>
 	);
 };
