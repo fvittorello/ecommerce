90ce150e54d79b6dbc1896e5fa2f6cc0dd35cb01
Fer Vittorello <fvittorello@gmail.com>
2020-10-26 18:09:48 -0300

    chore(reselect) improve memoization of the components state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -11,6 +12,7 @@ import { SignInAndSignUpPage } from 'pages/SignIn-SignUp';
 import Header from 'components/Header';
 import { auth, createUserProfileDocument } from 'firebase/firebase.utils';
 import { setCurrentUser } from 'redux/user/user.actions';
+import { selectCurrentUser } from 'redux/user/user.selector';
 
 const HatsPage = (props) => {
 	console.log(props);
@@ -74,8 +76,8 @@ class App extends React.Component {
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from 'firebase/firebase.utils';
 import CartIcon from 'components/CartIcon';
 import CartDropdown from 'components/CartDropdown';
+import { selectCartHidden } from 'redux/cart/cart.selector';
+import { selectCurrentUser } from 'redux/user/user.selector';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import './header.styles.scss';
@@ -38,9 +41,9 @@ const Header = ({ currentUser, hidden }) => {
 	);
 };
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-	currentUser,
-	hidden,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
+	hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
