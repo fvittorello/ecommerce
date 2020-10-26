d28d01fef46628b23a7a60a0396d0d541a93c78a
Fer Vittorello <fvittorello@gmail.com>
2020-10-26 00:08:01 -0300

    feat(CartItem) created the component and add it to the cart dropdown

--- src/components/CartDropdown/index.jsx
+++ src/components/CartDropdown/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { CustomButton } from 'components/CustomButton';
+import CartItem from 'components/CartItem';
 
 import './cart-dropdown.styles.scss';
 
-export const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
 	return (
 		<div className='cart-dropdown'>
-			<div className='cart-items' />
+			<div className='cart-items'>
+				{cartItems.map((cartItem) => (
+					<CartItem key={cartItem.id} item={cartItem} />
+				))}
+			</div>
 			<CustomButton>GO TO CHECKOUT</CustomButton>
 		</div>
 	);
 };
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from 'firebase/firebase.utils';
 import CartIcon from 'components/CartIcon';
-import { CartDropdown } from 'components/CartDropdown';
+import CartDropdown from 'components/CartDropdown';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import './header.styles.scss';
