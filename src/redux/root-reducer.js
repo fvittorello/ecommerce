f54f333d89ad9752d71d044815d28613a3fd751d
Fer Vittorello <fvittorello@gmail.com>
2020-10-28 21:24:49 -0300

    chore(Shop data) move data from component to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from 'redux/user/user.reducer';
 import cartReducer from 'redux/cart/cart.reducer';
 import directoryReducer from 'redux/directory/directory.reducer';
+import shopReducer from 'redux/shop/shop.reducer';
 
 const persistConfig = {
 	key: 'root',
@@ -16,6 +17,7 @@ const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 	directory: directoryReducer,
+	shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
