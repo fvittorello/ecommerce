cdeb8e674088052c02addc30475920c127bc5a9e
Fer Vittorello <fvittorello@gmail.com>
2020-10-28 21:07:17 -0300

    chore(Directory state) move state from component to redux

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './HomePage.styles.scss';
 
-import { Directory } from 'components/Directory';
+import Directory from 'components/Directory';
 
 export const HomePage = () => {
 	return (
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from 'redux/user/user.reducer';
 import cartReducer from 'redux/cart/cart.reducer';
+import directoryReducer from 'redux/directory/directory.reducer';
 
 const persistConfig = {
 	key: 'root',
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
+	directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
