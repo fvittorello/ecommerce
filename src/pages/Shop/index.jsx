d5a09b3796b8701abf756ad05f5b48beb9806f64
Fer Vittorello <fvittorello@gmail.com>
2020-10-29 04:02:13 -0300

    feat(Collection) add individual pages per collection

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from 'components/CollectionsOverview';
+import CollectionPage from 'pages/Collection';
 
-const Shop = ({ collections }) => {
+const Shop = ({ match }) => {
+	console.log(match);
 	return (
 		<div className='shop-page'>
-			<CollectionsOverview />
+			<Route exact path={`${match.path}`} component={CollectionsOverview} />
+			<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 		</div>
 	);
 };
