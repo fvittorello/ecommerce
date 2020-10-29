3ec5533bc548f275c9203503fe8c2f302158b24a
Fer Vittorello <fvittorello@gmail.com>
2020-10-29 12:41:19 -0300

    chore() change array to object to improve performance

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,5 +4,9 @@ const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
 
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) =>
+	Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = (collectionUrlParam) =>
 	createSelector([selectCollections], (collections) => collections[collectionUrlParam]);
