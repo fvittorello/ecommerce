f91d889a37c2b541b693b161c13b191da6d15446
Fer Vittorello <fvittorello@gmail.com>
2020-10-31 21:08:03 -0300

    chore(CartIcon) refactor style to styled-component

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -5,16 +5,15 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from 'redux/cart/cart.actions';
 import { selectCartItemsCount } from 'redux/cart/cart.selector';
 
-import { ReactComponent as ShoppingIcon } from 'assets/shopping-bag.svg';
-
-import './cart-icon.styles.scss';
+// Styles
+import { CartIconContainer, ShoppingIconContainer, ItemCountContainer } from './cart-icon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
 	return (
-		<div className='cart-icon' onClick={toggleCartHidden}>
-			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>{itemCount}</span>
-		</div>
+		<CartIconContainer onClick={toggleCartHidden}>
+			<ShoppingIconContainer />
+			<ItemCountContainer children={itemCount} />
+		</CartIconContainer>
 	);
 };
 
