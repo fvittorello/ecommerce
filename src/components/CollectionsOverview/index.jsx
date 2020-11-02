515386a1aa4c95e173e59b53030e4d29482def85
Fer Vittorello <fvittorello@gmail.com>
2020-11-01 23:29:24 -0300

    chore(CollectionsOverview) refactor style to styled-component

--- src/components/CollectionsOverview/index.jsx
+++ src/components/CollectionsOverview/index.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from 'components/CollectionPreview';
-
 import { selectCollectionsForPreview } from 'redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
+// Components
+import CollectionPreview from 'components/CollectionPreview';
+
+// Styled
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 const CollectionsOverview = ({ collections }) => {
 	return (
-		<div className='collections-overview'>
+		<CollectionsOverviewContainer>
 			{collections.map(({ id, ...otherCollectionProps }) => (
 				<CollectionPreview key={id} {...otherCollectionProps} />
 			))}
-		</div>
+		</CollectionsOverviewContainer>
 	);
 };
 
