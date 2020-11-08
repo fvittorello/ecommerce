110b3a243fdcdf0d119f4ffe47cc8eba223dfad6
Fer Vittorello <fvittorello@gmail.com>
2020-11-08 18:11:51 -0300

    chore(pages/Checkout) refactor styles to styled-component

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -2,47 +2,55 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { selectCartItems, selectCartTotal } from 'redux/cart/cart.selector';
+
+// Components
 import CheckoutItem from 'components/CheckoutItem';
 import StripeButton from 'components/StripeButton';
 
-import { selectCartItems, selectCartTotal } from 'redux/cart/cart.selector';
-
-import './checkout.styles.scss';
+// Styles
+import {
+	CheckoutPageContainer,
+	CheckoutHeaderContainer,
+	HeaderBlockContainer,
+	TotalContainer,
+	TestWarning,
+} from './checkout.styles';
 
 const Checkout = ({ cartItems, total }) => {
 	return (
-		<div className='checkout-page'>
-			<div className='checkout-header'>
-				<div className='header-block'>
+		<CheckoutPageContainer>
+			<CheckoutHeaderContainer>
+				<HeaderBlockContainer>
 					<span>Product</span>
-				</div>
-				<div className='header-block'>
+				</HeaderBlockContainer>
+				<HeaderBlockContainer>
 					<span>Description</span>
-				</div>
-				<div className='header-block'>
+				</HeaderBlockContainer>
+				<HeaderBlockContainer>
 					<span>Quantity</span>
-				</div>
-				<div className='header-block'>
+				</HeaderBlockContainer>
+				<HeaderBlockContainer>
 					<span>Price</span>
-				</div>
-				<div className='header-block'>
+				</HeaderBlockContainer>
+				<HeaderBlockContainer>
 					<span>Remove</span>
-				</div>
-			</div>
+				</HeaderBlockContainer>
+			</CheckoutHeaderContainer>
 			{cartItems.map((cartItem) => (
 				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 			))}
 
-			<div className='total'>
+			<TotalContainer>
 				<span>TOTAL: ${total}</span>
-			</div>
-			<div className='test-warning'>
+			</TotalContainer>
+			<TestWarning>
 				*Please use the following test credit card for payments*
 				<br />
 				4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-			</div>
+			</TestWarning>
 			<StripeButton price={total} />
-		</div>
+		</CheckoutPageContainer>
 	);
 };
 
