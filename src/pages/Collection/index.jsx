42703aa9546b20a7d62cf80e778238ba1f7c2f16
Fer Vittorello <fvittorello@gmail.com>
2020-11-08 17:54:36 -0300

    chore(pages/Collection) refactor style to styled-component

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CollectionItem from 'components/CollectionItem';
-
 import { selectCollection } from 'redux/shop/shop.selectors';
 
-import './collection.styles.scss';
+// Styles
+import { CollectionPageContainer, TitleContainer, ItemsContainer, CollectionItemContainer } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
 	const { title, items } = collection;
+
 	return (
-		<div className='collection-page'>
-			<h2 className='title'>{title}</h2>
-			<div className='items'>
+		<CollectionPageContainer>
+			<TitleContainer children={title} />
+			<ItemsContainer>
 				{items.map((item) => (
-					<CollectionItem key={item.id} item={item} />
+					<CollectionItemContainer key={item.id} item={item} />
 				))}
-			</div>
-		</div>
+			</ItemsContainer>
+		</CollectionPageContainer>
 	);
 };
 
