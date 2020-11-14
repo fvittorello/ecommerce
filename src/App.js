d4f9b9a78fff44a18a6850c2e4ce677a5bbc644a
Fer Vittorello <fvittorello@gmail.com>
2020-11-13 22:09:42 -0300

    feat(user) change emailSignIn to sagas

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { auth, createUserProfileDocument } from 'firebase/firebase.utils';
-import { setCurrentUser } from 'redux/user/user.actions';
 import { selectCurrentUser } from 'redux/user/user.selector';
 
 // Components
@@ -18,23 +16,20 @@ import Header from 'components/Header';
 import './App.css';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+	// constructor(props) {
+	// 	super(props);
 
-		this.state = {
-			currentUser: null,
-		};
-	}
+	// 	this.state = {
+	// 		currentUser: null,
+	// 	};
+	// }
 
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		const { setCurrentUser } = this.props;
-
 		// this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 		// 	if (userAuth) {
 		// 		const userRef = await createUserProfileDocument(userAuth);
-
 		// 		userRef.onSnapshot((snapShot) => {
 		// 			setCurrentUser({
 		// 				id: snapShot.id,
@@ -42,7 +37,6 @@ class App extends React.Component {
 		// 			});
 		// 		});
 		// 	}
-
 		// 	setCurrentUser(userAuth);
 		// });
 	}
@@ -74,8 +68,4 @@ const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -4,8 +4,7 @@ import { connect } from 'react-redux';
 import { FormInput } from 'components/FormInput';
 import { CustomButton } from 'components/CustomButton';
 
-import { auth, signInWithGoogle } from 'firebase/firebase.utils';
-import { googleSignInStart } from 'redux/user/user.actions';
+import { googleSignInStart, emailSignInStart } from 'redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -21,17 +20,10 @@ export class SignIn extends Component {
 
 	handleSubmit = async (event) => {
 		event.preventDefault();
-
+		const { emailSignInStart } = this.props;
 		const { email, password } = this.state;
 
-		try {
-			await auth.signInWithEmailAndPassword(email, password);
-			this.setState({ email: '', password: '' });
-		} catch (error) {
-			console.error(error);
-		}
-
-		this.setState({ email: '', password: '' });
+		emailSignInStart(email, password);
 	};
 
 	handleChange = (event) => {
@@ -81,6 +73,7 @@ export class SignIn extends Component {
 
 const mapDispatchToProps = (dispatch) => ({
 	googleSignInStart: () => dispatch(googleSignInStart()),
+	emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password })),
 });
 
 export default connect(null, mapDispatchToProps)(SignIn);
